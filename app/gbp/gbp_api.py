5d5c36dde8cde27f13de4119e94f7ba4fc86bc2d
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-06-14 20:12:47 -0300

    Update rolling distro information.

--- app/gbp/gbp_api.py
+++ app/gbp/gbp_api.py
@@ -1,6 +1,24 @@
-from requests import get
+from requests import get,post
 from json import loads
 
 def get_distro_information(distroId):
     url = "http://gbp-production.phx.gapinc.com/store-service/planning/store-plan/assortment-strategies/"
     return loads(get(url + distroId).text)["resource"]
+
+def get_rolling_distro(distroId, subClass, startDate, endDate):
+    url = "http://gbp-production.phx.gapinc.com/store-service/planning/in-season-store-plan/rolling-distro-store-lists/search"
+    payload = {
+          "rollingDistroStoreListRequest": {
+            "merchandiseHierarchyNode": subClass,
+            "timeBasedAssortmentStrategies": [
+              {
+                "startDate": startDate, 
+                "endDate": endDate,
+                "assortmentStrategyId": distroId
+              }
+            ]
+          }
+        }
+
+    result = post(url , json=payload).text
+    return loads(result)["resource"]    
--- app/gbp/gbp_transformer.py
+++ app/gbp/gbp_transformer.py
@@ -1,10 +1,27 @@
-from gbp_api import get_distro_information
+from gbp_api import get_distro_information, get_rolling_distro
 
 def transform_gbp(cc):
+    return {
+     "distros_information": transform_distro_information(cc),
+     "rolling_distro_stores": transform_rolling_distro(cc)
+    }
+
+def transform_distro_information(cc):
     distros_information = []
     for distro in cc["distros"]:
         distros_information.append(get_distro_information(distro["id"]))
 
-    return {
-     "distros_information": distros_information
-    }
+    return distros_information 
+
+def transform_rolling_distro(cc):
+    distros = []
+    for distro in cc["distros"]:
+        startDate = distro["inStoreStartDate"]
+        endDate = distro["markdownDate"]
+        rolling_distro = get_rolling_distro(distro["id"], cc["subClassId"], startDate, endDate)
+        distro["rollingDistro"] = rolling_distro
+            
+        distros.append(distro)
+        
+    return distros
+
