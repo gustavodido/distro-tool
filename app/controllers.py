77cac6e5550ff41b50993495cc7ffd416bcb28c8
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-06-14 14:17:35 -0300

    Add post action handler.

--- app/controllers.py
+++ app/controllers.py
@@ -4,4 +4,8 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", has_results=False)
+
+@app.route('/', methods=['POST'])
+def search():
+    return render_template("index.html", has_results=True)
